65541534a2423b59a2f47d2dbd49337ee17523e1
coursju <coursju@outlook.com>
2021-09-16 15:39:00 +0400

    added dsp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -93,8 +93,13 @@ void EqTrainingAudioProcessor::changeProgramName (int index, const juce::String&
 //==============================================================================
 void EqTrainingAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    juce::dsp::ProcessSpec spec;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = 1;
+    spec.sampleRate = sampleRate;
+    
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
 }
 
 void EqTrainingAudioProcessor::releaseResources()
@@ -135,27 +140,22 @@ void EqTrainingAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
+
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    juce::dsp::AudioBlock<float> block = buffer;
+    
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+    
+    juce::dsp::ProcessContextReplacing<float> leftContext = leftBlock;
+    juce::dsp::ProcessContextReplacing<float> rightContext = rightBlock;
+    
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
 
-        // ..do something to the data...
-    }
+
 }
 
 //==============================================================================
